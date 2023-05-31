32358b82bf1fee054179e6d22e44c1bdd31f2505
Joy <parkerjn90@gmail.com>
2023-05-31 14:24:02 -0400

    add event handler to log checked state, converted TodoItem to client component, and adde defaultChecked to persist whether component had been checked or not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import { prisma } from '@/db';
 import Link from 'next/link';
 import TodoItem from './components/TodoItem.tsx';
 
-function getTodos () {
+function getTodos() {
   return prisma.todo.findMany()
 }
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
 export default async function Home() {
 
 
@@ -27,7 +34,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo}/>
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
