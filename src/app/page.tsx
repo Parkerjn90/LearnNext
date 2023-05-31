d3e481128a7bc1e9b619a968c3be25f64294a528
Joy <parkerjn90@gmail.com>
2023-05-31 12:17:07 -0400

    add test data to db, add async function, await prisma model query set to variable to act on within the component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+import { prisma } from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
+
   return (
     <>
-    <header>
-      <h1 className="text-2xl">Todos</h1>
-      <Link href="/new">New</Link>
-    </header>
-    <ul></ul>
+      <header
+        className="flex
+        justify-between
+        mb-4
+        items-center">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href="/new"
+          className="border border-slate-300
+        text-slate-300 px-2 py-1 rounded
+        hover:bg-slate-700 focu-within:bg-slate-700
+        outline-none">New</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
