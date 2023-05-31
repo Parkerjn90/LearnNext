6eeb5d1ae22eaabcafaf05552e42eebf024ed503
Joy <parkerjn90@gmail.com>
2023-05-31 12:00:28 -0400

    add link to route to 'new' folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
     <header>
       <h1 className="text-2xl">Todos</h1>
+      <Link href="/new">New</Link>
     </header>
     <ul></ul>
     </>
