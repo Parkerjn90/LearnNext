40c9c04cc1cc88de2a565f20e1f7edd3c5d824ca
Joy <parkerjn90@gmail.com>
2023-05-31 12:42:10 -0400

    add cancel button to return to main page, and create button to submit form (need to handle that event still)

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Page() {
   return (
     <>
@@ -8,10 +10,21 @@ export default function Page() {
         items-center">
         <h1 className="text-2xl">New</h1>
         <form className="flex gap-2 flex-col">
-          <input type="text" name="title" className="border border-slate-300
+          <input type="text" name="title"
+            className="border bg-transparent rounded px-2 py-1
+          outline-none focus-within:border-slate-100"/>
+          <div className="flex gap-1 justify-end">
+            <Link href=".."
+              className="border border-slate-300
+          text-slate-300 px-2 py-1 rounded
+          hover:bg-slate-700 focus-within:bg-slate-700
+          outline-none">Cancel</Link>
+            <button type="submit"
+              className="border border-slate-300
         text-slate-300 px-2 py-1 rounded
         hover:bg-slate-700 focus-within:bg-slate-700
-        outline-none"/>
+        outline-none">Create</button>
+          </div>
         </form>
       </header>
     </>
