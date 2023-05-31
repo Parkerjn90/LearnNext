2382bacc8c2f647f21b77cb672c3a4ffa5b30577
Joy <parkerjn90@gmail.com>
2023-05-31 13:17:21 -0400

    add get request to form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,6 +1,22 @@
+import { prisma } from '@/db';
+import { redirect } from 'next/navigation';
 import Link from 'next/link';
+import { type } from 'os';
+
+async function createTodo(data: FormData) {
+  "use server"
+
+  const title = data.get("title")?.valueOf()
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid title")
+  }
+
+  await prisma.todo.create({ data: {title, complete: false} })
+  redirect("/")
+}
 
 export default function Page() {
+
   return (
     <>
       <header
@@ -9,7 +25,8 @@ export default function Page() {
         mb-4
         items-center">
         <h1 className="text-2xl">New</h1>
-        <form className="flex gap-2 flex-col">
+        <form action={createTodo}
+        className="flex gap-2 flex-col">
           <input type="text" name="title"
             className="border bg-transparent rounded px-2 py-1
           outline-none focus-within:border-slate-100"/>
